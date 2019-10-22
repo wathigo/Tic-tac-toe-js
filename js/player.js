e81f95fcd4001607e96481f43ce330377a8273a3
wathigo <wathigosimon@gmail.com>
2019-10-22 09:55:47 -0400

    Refactor code

--- js/player.js
+++
@@ -1,3 +0,0 @@
-export const player = (name, mark) => {
-  return {name, mark};
-};
--- js/script.js
+++ js/script.js
@@ -1,14 +1,34 @@
-import displayController as dc from './js/display_controller';
-import gameBoard as gboard from './js/game_board';
-import player as ply from './js/player';
+const gameBoard = (() => {
+  let board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const addPosition = (position, mark) => {
+    board[position-1] = mark;
+  };
+  return {
+    board,
+    addPosition
+  }
+})();
 
-var render = () => {
+const player = (name, mark) => {
+  return {name, mark};
+};
+
+const displayController = () => {
+  let players;
+  const start_game = (player1, player2) => {
+    players = [player1, player2];
+  };
+
+  const switch_players = () => {
+    [players[0], players[1]] = [players[1], players[0]];
+  }
+}
+
+const render = () => {
 
     document.querySelectorAll('.boardbox').forEach(function (box, index){
-        box.value = gboard.board[index]
+        console.log(box);
+        box.textContent = gameBoard.board[index]
     })
 
 };
-(function main() {
-    render();
-  }();
