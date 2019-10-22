105e495a80f1196b97bf194c2f53a15aa69e8998
samuel <samuelgarciacompanys@gmail.com>
2019-10-22 17:18:55 +0200

    player forms added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const gameBoard = (() => {
-  let board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let board = ["","","","","","","","",""];
+
   const addPosition = (position, mark) => {
     board[position-1] = mark;
   };
@@ -13,22 +14,43 @@ const player = (name, mark) => {
   return {name, mark};
 };
 
-const displayController = () => {
-  let players;
+const displayController = (() => {
+  var players = [];
   const start_game = (player1, player2) => {
-    players = [player1, player2];
+    var players = [player1, player2];
   };
 
   const switch_players = () => {
     [players[0], players[1]] = [players[1], players[0]];
   }
+
+  return{start_game, switch_players, players};
+})();
+
+const updateBox = (box) => {
+  console.log('click')
+  gameBoard.addPosition(box.textContent, displayController.players[0].mark)
+  console.log(gameBoard.board)
+  box.textContent = 'X'
+  displayController.switch_players();
+}
+
+const createPlayers = () =>{
+    const playerO =  player(document.querySelector('.playerO').value, "O")
+    const playerX =  player(document.querySelector('.playerX').value, "X")
+    displayController.start_game(playerO, playerX)
+
 }
 
 const render = () => {
 
+    createPlayers();
     document.querySelectorAll('.boardbox').forEach(function (box, index){
         console.log(box);
         box.textContent = gameBoard.board[index]
+        box.addEventListener('click', (evt) => {
+          updateBox(event.target);
+        })
     })
 
 };
