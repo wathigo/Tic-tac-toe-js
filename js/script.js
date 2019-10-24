d7ce4caccd53c11542cd7bb752832541a28b922a
wathigo <wathigosimon@gmail.com>
2019-10-24 08:18:02 -0400

    Fix linter errors

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ const gameBoard = (() => {
   };
 
   const resetBoard = () => {
-     board = ['1', '4', '7', '2', '5', '8', '3', '6', '9'];
-     return board;
+    board = ['1', '4', '7', '2', '5', '8', '3', '6', '9'];
+    return board;
   };
   return {
     board,
@@ -19,105 +19,102 @@ const gameBoard = (() => {
 const reload = () => {
   location.reload();
 };
-const player = (name, mark) => {
-  return { name, mark };
-};
+const player = (name, mark) => ({ name, mark });
 
 const displayController = (() => {
-
   const players = [];
   let gamestop = '';
   let rolls = 0;
 
-  const start_game = (player1, player2) => {
+  const startGame = (player1, player2) => {
     players[0] = player1;
     players[1] = player2;
     gamestop = false;
     rolls = 0;
   };
 
-  const switch_players = (player1, player2) => {
-    if(document.querySelector('.winmsg b').textContent.length === 0 
-          && document.querySelector('.errormsg b').textContent.length === 0){
+  const switchPlayers = (player1, player2) => {
+    if (document.querySelector('.winmsg b').textContent.length === 0
+          && document.querySelector('.errormsg b').textContent.length === 0) {
       document.querySelector('.playersturn').style.visibility = 'visible';
       document.querySelector('.playersturn').textContent = `${player2.name} turn`;
       players[0] = player2;
       players[1] = player1;
     } else {
       document.querySelector('.playersturn').style.visibility = 'hidden';
-      }
-
-  }
-
-  const check_winner = (board, player) => {
-    check_horizontal(board, player)
-    check_vertical(board, player)
-    check_diagonal(board, player)
-    rolls += 1
-    if(rolls == 9){
-    check_draw(board) }
-  }
-
-  const check_horizontal = (board, player) => {
-    for (let i = 0; i < 3; i++) {
-      if (board[0 + i * 3] == board[1 + i * 3] && board[1 + i * 3] == board[2 + i * 3]) {
-        winner(player)
-      }
     }
-  }
+  };
 
-  const check_vertical = (board, player) => {
-    for (let i = 0; i < 3; i++) {
-      if (board[0 + i] == board[3 + i] && board[3 + i] == board[6 + i]) {
-        winner(player)
-      }
-    }
-  }
-
-  const check_diagonal = (board, player) => {
-    if (board[0] == board[4] && board[4] == board[8]) {
-      winner(player) }
-
-    if (board[6] == board[4] && board[4] == board[2]) {
-      winner(player)
-    }
-  }
-
-  const check_draw = (board) => {
-     if(gamestop == false){
-       gamestop = true;
-       draw();
-     }
-
-  }
-
-  const winner = (player) => {
-    gamestop = true
-    document.querySelector('.winmsg b').textContent = `${player.name} won!`
+  const winner = () => {
+    gamestop = true;
+    document.querySelector('.winmsg b').textContent = `${displayController.players[0].name} won!`;
     document.querySelector('.winmsg').style.visibility = 'visible';
-    document.querySelectorAll('.boardbox').forEach(function (box, index){
-      let newBox = box.cloneNode(true);
+    document.querySelectorAll('.boardbox').forEach((box) => {
+      const newBox = box.cloneNode(true);
       box.parentNode.replaceChild(newBox, box);
-    })
-  }
+    });
+  };
+
+  const checkHorizontal = (board) => {
+    for (let i = 0; i < 3; i += 1) {
+      if (board[0 + i * 3] === board[1 + i * 3] && board[1 + i * 3] === board[2 + i * 3]) {
+        winner();
+      }
+    }
+  };
+
+  const checkVertical = (board) => {
+    for (let i = 0; i < 3; i += 1) {
+      if (board[0 + i] === board[3 + i] && board[3 + i] === board[6 + i]) {
+        winner();
+      }
+    }
+  };
+
+  const checkDiagonal = (board) => {
+    if (board[0] === board[4] && board[4] === board[8]) {
+      winner();
+    }
+
+    if (board[6] === board[4] && board[4] === board[2]) {
+      winner();
+    }
+  };
 
   const draw = () => {
-    gamestop = true
-    document.querySelector('.winmsg b').textContent = `It's a draw`
+    gamestop = true;
+    document.querySelector('.winmsg b').textContent = 'It\'s a draw';
     document.querySelector('.winmsg').style.visibility = 'visible';
-    document.querySelectorAll('.boardbox').forEach(function (box, index){
-      let newBox = box.cloneNode(true);
+    document.querySelectorAll('.boardbox').forEach((box) => {
+      const newBox = box.cloneNode(true);
       box.parentNode.replaceChild(newBox, box);
-    })
-  }
+    });
+  };
 
-  return { start_game, switch_players, players, check_winner, rolls };
+  const checkDraw = () => {
+    if (gamestop === false) {
+      gamestop = true;
+      draw();
+    }
+  };
+
+  const checkWinner = (board) => {
+    checkHorizontal(board);
+    checkVertical(board);
+    checkDiagonal(board);
+    rolls += 1;
+    if (rolls === 9) {
+      checkDraw();
+    }
+  };
+
+  return {
+    startGame, switchPlayers, players, checkWinner, rolls,
+  };
 })();
 
 const updateBox = (box) => {
-  console.log(box.textContent)
-  if (box.textContent === "X" || box.textContent === "O") {
-    console.log("Not empty!")
+  if (box.textContent === 'X' || box.textContent === 'O') {
     document.querySelector('.playersturn').style.visibility = 'hidden';
     document.querySelector('.errormsg').style.visibility = 'visible';
     document.querySelector('.errormsg b').textContent = 'Invalid move!';
@@ -127,70 +124,59 @@ const updateBox = (box) => {
       document.querySelector('.playersturn').style.visibility = 'visible';
     }, 800);
   } else {
-    console.log(displayController.players)
-    gameBoard.addPosition(box.textContent, displayController.players[0].mark)
-    console.log(gameBoard.board)
+    gameBoard.addPosition(box.textContent, displayController.players[0].mark);
     box.textContent = displayController.players[0].mark;
-    if(box.textContent === 'X'){
+    if (box.textContent === 'X') {
       box.style.background = '#F0E68C';
     } else {
       box.style.background = '#FFFACD';
     }
-    console.log(displayController.players)
-    displayController.check_winner(gameBoard.board, displayController.players[0]);
-    displayController.switch_players(displayController.players[0], displayController.players[1]);
+    displayController.checkWinner(gameBoard.board);
+    displayController.switchPlayers(displayController.players[0], displayController.players[1]);
   }
-}
+};
 
 const newGame = () => {
   document.querySelector('.winmsg').style.visibility = 'hidden';
   document.querySelector('.winmsg b').textContent = '';
   gameBoard.board = gameBoard.resetBoard();
-  document.querySelectorAll('.boardbox').forEach(function (box, index){
+  document.querySelectorAll('.boardbox').forEach((box) => {
     box.style.background = '#fff';
-  })
+  });
   render();
-}
+};
 
 const createPlayers = () => {
-  if(document.querySelector('.playerO').value.length != 0 && document.querySelector('.playerO').value.length != 0){
-    const playerO = player(document.querySelector('.playerO').value, "O")
-    const playerX = player(document.querySelector('.playerX').value, "X")
-    displayController.start_game(playerO, playerX)
-    return true
-  }else{
-    return false
+  if (document.querySelector('.playerO').value.length !== 0 && document.querySelector('.playerO').value.length !== 0) {
+    const playerO = player(document.querySelector('.playerO').value, 'O');
+    const playerX = player(document.querySelector('.playerX').value, 'X');
+    displayController.startGame(playerO, playerX);
+    return true;
   }
-}
+  return false;
+};
 
 const startGame = () => {
-  if(createPlayers()){
-
+  if (createPlayers()) {
     document.querySelector('.boardcont').style.display = 'block';
     document.querySelector('.gamestarter').style.display = 'none';
-      // document.querySelectorAll('.boardrow').forEach(row => {
+    // document.querySelectorAll('.boardrow').forEach(row => {
   //   row.style.display = 'block';
   // });
-  }else{
-
-
+  } else {
     document.querySelector('.playernames').style.visibility = 'visible';
     setTimeout(() => {
       document.querySelector('.playernames').style.visibility = 'hidden';
     }, 800);
-
-
-} }
+  }
+};
 
 const render = () => {
   createPlayers();
-  document.querySelectorAll('.boardbox').forEach(function (box, index) {
-    console.log(box);
-    box.textContent = gameBoard.board[index]
-    console.log(box.textContent)
+  document.querySelectorAll('.boardbox').forEach((box, index) => {
+    box.textContent = gameBoard.board[index];
     box.addEventListener('click', (evt) => {
       updateBox(event.target);
-    })
-  })
-
+    });
+  });
 };
