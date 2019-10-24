6d5da1875fb2eb2c2b85cc6d84d080dbe1c719f1
samuel <samuelgarciacompanys@gmail.com>
2019-10-24 17:10:33 +0200

    linters

--- js/gameBoard.js
+++ js/gameBoard.js
@@ -1,19 +1,18 @@
 const gameBoard = (() => {
-    let board = ['1', '4', '7', '2', '5', '8', '3', '6', '9'];
-  
-    const addPosition = (position, mark) => {
-      board[position - 1] = mark;
-    };
-  
-    const resetBoard = () => {
-      board = ['1', '4', '7', '2', '5', '8', '3', '6', '9'];
-      return board;
-    };
-    return {
-      board,
-      addPosition,
-      resetBoard,
-    };
-  })();
+  let board = ['1', '4', '7', '2', '5', '8', '3', '6', '9'];
+  const addPosition = (position, mark) => {
+    board[position - 1] = mark;
+  };
 
-  export default gameBoard;
+  const resetBoard = () => {
+    board = ['1', '4', '7', '2', '5', '8', '3', '6', '9'];
+    return board;
+  };
+  return {
+    board,
+    addPosition,
+    resetBoard,
+  };
+})();
+
+export default gameBoard;
